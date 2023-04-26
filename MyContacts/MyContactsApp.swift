b2218f3ff10d0261f1b43dc993f30fa91f1ffdae
JH Kim <kimiyo@naver.com>
2023-04-26 20:48:30 +0900

    Initial Commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by JongHoon Kim on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
